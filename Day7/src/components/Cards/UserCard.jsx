c20bfab51944409bf2548515b1e3591dce8775e0
Chintan <chintanwork21@gmail.com>
2026-04-21 15:50:11 +0530

    Task Completed

--- Day7/src/components/Cards/UserCard.jsx
+++ Day7/src/components/Cards/UserCard.jsx
@@ -1,19 +1,24 @@
 // UserCard.jsx
-import React from 'react'
+import React from "react";
 function UserCard({ user }) {
-  return (  
+  return (
     <div className="w-full mx-auto bg-gradient-to-r from-zinc-100 to-zinc-200 dark:from-[#000000] dark:to-[#0a0d37] border-zinc-300 dark:border-[#1b2c68a0] relative rounded-lg border shadow-lg p-4 cursor-pointer transition-transform transform hover:scale-105 hover:shadow-2xl hover:border-blue-500 dark:hover:border-blue-500">
-    <div className="user-card">
-      <img src={user.avatar_url} alt={user.login} className="avatar" />
-      <div className="user-info">
-        <h3>Name :{user.login}</h3>
+      <div className="user-card">
+        <img src={user.avatar_url} alt={user.login} className="avatar" />
+        <div className="user-info">
+          <h3>Name:{user.login}</h3>
+        </div>
+        <a
+          href={user.html_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="profile-link"
+        >
+          View Profile
+        </a>
       </div>
-      <a href={user.html_url} target="_blank" rel="noopener noreferrer" className="profile-link">
-        View Profile
-      </a>
     </div>
-    </div>
-  )
+  );
 }
 export default UserCard;
 
--- Day7/src/components/UserList.jsx
+++ Day7/src/components/UserList.jsx
@@ -7,10 +7,11 @@ function UserList() {
   const [users, setUsers] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [count, setCount] = useState(30)
     useEffect(() => {   
         async function fetchUsers() {
             try {
-                const response = await fetch('https://api.github.com/users')    
+                const response = await fetch(`https://api.github.com/users?per_page=${count}`)    
                 if (!response.ok) {
                     throw new Error('Failed to fetch users')
                 }
@@ -23,7 +24,7 @@ function UserList() {
             }
         }
         fetchUsers()
-    }, [])
+    }, [count])
 
     if (loading) {
         return <Loader />
@@ -34,11 +35,15 @@ function UserList() {
     }
 
     return (
+        <>
+        <h1 className="text-2xl font-bold mb-4 text-indigo-800 bg-amber-300 text-center cursor-pointer hover:scale-105">GitHub Users</h1>
+        <input type='number' value={count} onChange={(e) => setCount(e.target.value)}></input>
         <div className="user-list">
             {users.map((user) => (
                 <UserCard key={user.id} user={user} />
             ))}
         </div>
+        </>
     )       
 }
 
