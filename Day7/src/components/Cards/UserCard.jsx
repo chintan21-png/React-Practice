96679a69e8fb75137feac967a383c1e14dac5ef6
Chintan <chintanwork21@gmail.com>
2026-04-21 12:56:59 +0530

    Updated

--- Day7/src/components/Cards/UserCard.jsx
+++ Day7/src/components/Cards/UserCard.jsx
@@ -2,11 +2,22 @@
 import React from 'react'
 function UserCard({ user }) {
   return (  
+    <div className="w-full mx-auto bg-gradient-to-r from-zinc-100 to-zinc-200 dark:from-[#000000] dark:to-[#0a0d37] border-zinc-300 dark:border-[#1b2c68a0] relative rounded-lg border shadow-lg p-4 cursor-pointer transition-transform transform hover:scale-105 hover:shadow-2xl hover:border-blue-500 dark:hover:border-blue-500">
     <div className="user-card">
       <img src={user.avatar_url} alt={user.login} className="avatar" />
-      <h3>{user.login}</h3>
-      
+      <div className="user-info">
+        <h3>Name :{user.login}</h3>
+      </div>
+      <a href={user.html_url} target="_blank" rel="noopener noreferrer" className="profile-link">
+        View Profile
+      </a>
+    </div>
     </div>
   )
 }
 export default UserCard;
+
+//what is rel="noopener noreferrer" in the anchor tag?
+//The rel="noopener noreferrer" attribute in the anchor tag is used for security reasons when opening links in a new tab (target="_blank").
+//noopener: This prevents the new page from being able to access the window.opener property, which can be a security risk if the new page is malicious. It helps protect against phishing attacks and other malicious activities.
+//noreferrer: This prevents the browser from sending the referrer information to the new page. This can help protect user privacy by not revealing the source of the traffic to the new page.
--- Day7/src/components/Loader.jsx
+++ Day7/src/components/Loader.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Loader() {
   return (
     <div className="loader">
+      <svg class="w-8 h-8 text-orange-600 animate-spin" viewBox="0 0 24 24"><path fill="currentColor" d="M6 2v6h.01L12 12l5.99-4H18V2H6zm0 20h12v-6h-.01L12 12l-5.99 4H6v6z"/></svg>  
       <p>Loading...</p>
     </div>
   )
