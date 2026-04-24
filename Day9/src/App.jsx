2a153fda429d06762aef6368ff30b446b8ab4d35
Chintan <chintanwork21@gmail.com>
2026-04-24 12:42:16 +0530

    Day9 Updated

--- Day9/src/App.jsx
+++ Day9/src/App.jsx
@@ -1,10 +1,35 @@
-import UserList from "./components/UserList";
+// import UserList from "./components/UserList";
+// function App() {
+//   return (
+//     <div className="App">
+//       <UserList />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//useMemo hook
+import { useState } from "react"
+import Sum from "./Sum";
+
 function App() {
-  return (
-    <div className="App">
-      <UserList />
-    </div>
-  );
-}
+    const [count, setCount] = useState(0)
+    const [number, setNumber] = useState(0);
+    console.log("app render")
+    return(
+        <>
+            <h1>Counter: {count}</h1>
+            <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
+            <h2>Your current Number: {number}</h2>
+            <button onClick={() => setNumber(prev => prev + 100)}>Increment Number</button>
+            <Sum number={number}/>
+        </>
+    );
+};
 
 export default App;
+
+
+
+
---
+++ Day9/src/Sum.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Sum = React.memo((number) => {
+     function calculateSum() {
+        let sum = 0;
+
+        for (let i=0; i <= number; i++) {
+            sum+=i;
+        }
+        return sum;
+    }
+
+    const total = calculateSum();
+    console.log("Sum render")
+
+    return(
+        <>
+            <h1>This is out Math Library</h1>
+            <h2>Sum: {total}</h2>
+        </>
+    );
+})
+
+export default Sum;
