c309e332d4b8a4a1a948d6b54470872f5aa993e6
Chintan <chintanwork21@gmail.com>
2026-04-30 17:21:56 +0530

    Updated

--- Day10/src/App.jsx
+++ Day10/src/App.jsx
@@ -1,40 +1,42 @@
-// //import React from "react";
+// // //import React from "react";
 
-// function App() {
+// // function App() {
+// //   return(
+// //     <div>
+// //       <h1 className="text-3xl font-bold underline"> Hii</h1>
+// //     </div>
+// //   )
+// // }
+
+// // export default App;
+
+// //useContext
+// import { createContext, useState } from "react";
+// import Header from "./components/Header";
+// import Body from "./components/Body";
+// import Footer from "./components/Footer";
+// //import Counter from "./components/Counter";
+
+// // export const CounterContext = createContext();
+//   export const ProductContext = createContext();
+// function App () {
+
+//   //const [count, setCount] = useState(0);
+//   const [cartItem, setCartItem] = useState(0);
+  
 //   return(
-//     <div>
-//       <h1 className="text-3xl font-bold underline"> Hii</h1>
-//     </div>
-//   )
-// }
+//     <>
+//     {/* <CounterContext value={{count, setCount}}> */}
+//     <ProductContext value={{cartItem, setCartItem}}>
+//       <Header />
+//       <Body/>
+//       <Footer />
+//     </ProductContext>
+//     {/* </CounterContext> */}
+//     </>
+//   );
+// };
 
 // export default App;
 
-//useContext
-import { createContext, useState } from "react";
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-//import Counter from "./components/Counter";
 
-// export const CounterContext = createContext();
-  export const ProductContext = createContext();
-function App () {
-
-  //const [count, setCount] = useState(0);
-  const [cartItem, setCartItem] = useState(0);
-  
-  return(
-    <>
-    {/* <CounterContext value={{count, setCount}}> */}
-    <ProductContext value={{cartItem, setCartItem}}>
-      <Header />
-      <Body/>
-      <Footer />
-    </ProductContext>
-    {/* </CounterContext> */}
-    </>
-  );
-};
-
-export default App;
