80448cc1d1087c4acbbb5737c03a8cd5a538a5cc
Chintan <chintanwork21@gmail.com>
2026-05-01 13:04:09 +0530

    Kanban-Frontend Started

--- index.js
+++ index.js
@@ -17,21 +17,25 @@
 
 // main();
 
-const axios = require("axios");
+// const axios = require("axios");
 
-async function main() {
-    const response = await axios.post("https://httpdump.app/dumps/8f55c4ec-0beb-4969-8fb0-3e35f2f8ee3d",
-        {
-            username: "chintan",
-            password: "123456",
-        },
-        {
-            headers: {
-                Authorization: "Bearer 123"
-            },
-        },
-    );
-    console.log(response.data);
-};
+// async function main() {
+//     const response = await axios.post("https://httpdump.app/dumps/8f55c4ec-0beb-4969-8fb0-3e35f2f8ee3d",
+//         {
+//             username: "chintan",
+//             password: "123456",
+//         },
+//         {
+//             headers: {
+//                 Authorization: "Bearer 123"
+//             },
+//         },
+//     );
+//     console.log(response.data);
+// };
 
-main();
+// main();
+
+const nums = [1,2,4,5,6];
+const sum = nums.reduce((acc,curr) => acc + curr, 0);
+console.log(sum)
